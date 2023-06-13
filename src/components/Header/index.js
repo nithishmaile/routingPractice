bb5eeca415bb80c0ccd900ba2c1cd43233d69edc
nithishkumar <nithishmaile@gmail.com>
2023-06-13 08:46:44 +0000

    output is not displaying

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+// Write your JS code here
+
+import './index.css'
+
+const Header = () => {
+  ;<div className="bg-container">
+    <nav className="card-container">
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+          alt="wave"
+        />
+        <p>Wave</p>
+      </div>
+      <div>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+    </nav>
+  </div>
+}
+
+export default Header
